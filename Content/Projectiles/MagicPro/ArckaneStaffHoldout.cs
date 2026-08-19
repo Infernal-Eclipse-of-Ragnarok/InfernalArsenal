c2fdfbf9bc4ad1d779724f0545cf5c2931e6fc01
closet <d5ac99@protonmail.com>
2026-08-19 11:22:54 -0400

    arckanestaff resprite and rework

---
+++ Content/Projectiles/MagicPro/ArckaneStaffHoldout.cs
@@ -0,0 +1,154 @@
+﻿using CalamityMod.Buffs.DamageOverTime;
+using CalamityMod.Buffs.StatDebuffs;
+using InfernalEclipseWeaponsDLC.Content.Items.Weapons.Magic;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static CatalystMod.Effects.Begin;
+using static Daybreak.Common.Features.Hooks.GlobalItemHooks;
+
+namespace InfernalEclipseWeaponsDLC.Content.Projectiles.MagicPro
+{
+    public class ArckaneStaffHoldout : ModProjectile
+    {
+        public ref float HoldTimer => ref Projectile.ai[0];
+        public ref float ShootTimer => ref Projectile.ai[1];
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 5;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 70;
+            Projectile.height = 62;
+            Projectile.aiStyle = ProjAIStyleID.HeldProjectile;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.friendly = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.penetrate = -1;
+            Projectile.scale = 1f;
+            Projectile.DamageType = DamageClass.Magic;
+            DrawOffsetX = 20;
+            DrawOriginOffsetY = 16;
+
+        }
+
+        public override bool? CanDamage() => false;
+        public override bool? CanHitNPC(NPC target)
+        {
+            return false;
+        }
+        public override bool PreAI()
+        {
+            Player player = Main.player[Projectile.owner];
+            Vector2 playerCenter = player.RotatedRelativePoint(player.MountedCenter);
+
+            HoldTimer += 1f;
+            --Projectile.soundDelay;
+
+            if (Projectile.soundDelay <= 0)
+            {
+                if (HoldTimer >= 25f)
+                {
+                    SoundEngine.PlaySound(SoundID.Item43, Projectile.position);
+                    Projectile.soundDelay = 50;
+                }
+            }
+
+            Projectile.frameCounter += 1;
+            if (Projectile.frameCounter >= 5)
+            {
+                Projectile.frameCounter = 0;
+                Projectile.frame = ++Projectile.frame % Main.projFrames[Type];
+            }
+
+            Projectile.timeLeft = 60;
+            player.SetDummyItemTime(25);
+
+            ShootTimer += 1f;
+            bool shouldShootArrow = false;
+            int initialShootDelay = 24;
+            int shootDelayAdjustmentRate = 6;
+
+            if (ShootTimer >= initialShootDelay - shootDelayAdjustmentRate)
+            {
+                ShootTimer = 0f;
+                shouldShootArrow = true;
+                Main.LocalPlayer.CheckMana(Main.LocalPlayer.HeldItem, -1, true, false);
+            }
+            else if (ShootTimer <= 1 && HoldTimer <=1)
+            {
+                shouldShootArrow = true;
+            }
+
+            if (shouldShootArrow && Main.myPlayer == Projectile.owner)
+            {
+                if (player.channel)
+                {
+                    float holdoutDistance = ArckaneStaff.HoldoutDistance * Projectile.scale;
+                    Vector2 holdoutOffset = holdoutDistance * Vector2.Normalize(Main.MouseWorld - playerCenter);
+                    if (holdoutOffset.X != Projectile.velocity.X || holdoutOffset.Y != Projectile.velocity.Y)
+                    {
+                        Projectile.netUpdate = true;
+                    }
+                    Projectile.velocity = holdoutOffset;
+
+                    for (int j = 0; j < 1; j++)
+                    {
+                        var spawnLocation = playerCenter + holdoutOffset + Main.rand.NextVector2Circular(6, 6);
+                        var source = player.GetSource_ItemUse(player.HeldItem);
+                        var projectileA = Projectile.NewProjectile(source, spawnLocation, Vector2.Normalize(Projectile.velocity) * (float)Math.Exp(2.5), ModContent.ProjectileType<ArckaneGemPro>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                        Projectile.NewProjectile(source, spawnLocation, Vector2.Normalize(Projectile.velocity) * (float)Math.Exp(2.5), ModContent.ProjectileType<ArckaneScythe1Pro>(), 50, Projectile.knockBack, Projectile.owner, ai2: projectileA);
+                        Projectile.NewProjectile(source, spawnLocation, Vector2.Normalize(Projectile.velocity) * (float)Math.Exp(2.5), ModContent.ProjectileType<ArckaneScythe2Pro>(), 50, Projectile.knockBack, Projectile.owner, ai2: projectileA);
+                    }
+                }
+                else
+                {
+                    Projectile.Kill();
+                    return false;
+                }
+            }
+            if (Main.LocalPlayer.CheckMana(Main.LocalPlayer.HeldItem, -1) == false && HoldTimer >= 15)
+            {
+                Projectile.Kill();
+                return false;
+            }
+
+            if (Projectile.velocity.X > 0f)
+            {
+                player.ChangeDir(1);
+            }
+            else if (Projectile.velocity.X < 0f)
+            {
+                player.ChangeDir(-1);
+            }
+
+            Projectile.spriteDirection = Projectile.direction;
+            player.heldProj = Projectile.whoAmI;
+            Projectile.Center = playerCenter + Projectile.rotation.ToRotationVector2().RotatedBy(-MathHelper.PiOver2) * 20;
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            player.itemRotation = (Projectile.velocity * Projectile.direction).ToRotation();
+            return false;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            if (Projectile.owner == Main.myPlayer)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int i = 0; i < ArckaneGemPro.dusts.Length; i++)
+                    {
+                        Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ArckaneGemPro.dusts[i], 0, 0, 100, default, 2f).noGravity = true;
+                    }
+                }
+
+            }
+        }
+    }
+}
--- Content/Projectiles/MagicPro/ArckaneStaffPro.cs
+++ Content/Projectiles/MagicPro/ArckaneStaffPro.cs
@@ -1,16 +1,22 @@
 ﻿using CalamityMod.Buffs.DamageOverTime;
 using CalamityMod.Buffs.StatDebuffs;
+using CalamityMod.Dusts;
+using CalamityMod.Dusts.WaterSplash;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.Audio;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static CatalystMod.Effects.Begin;
 
 namespace InfernalEclipseWeaponsDLC.Content.Projectiles.MagicPro
 {
-    public class ArckaneStaffPro : ModProjectile
+    public class ArckaneGemPro : ModProjectile
     {
-        private static readonly int[] dusts = new int[5]
+        public static readonly int[] dusts = new int[5]
         {
             59,
             60,
@@ -21,10 +27,18 @@ namespace InfernalEclipseWeaponsDLC.Content.Projectiles.MagicPro
 
         private const float RotationOffset = 0f;
 
+        private bool storedVelocity = false;
+        private float originalSpeed;
+        private float speedTimer = 0f;
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Type] = 5;
+            ProjectileID.Sets.TrailingMode[Type] = 0;
+        }
         public override void SetDefaults()
         {
-            Projectile.width = 56;
-            Projectile.height = 34;
+            Projectile.width = 13;
+            Projectile.height = 23;
             Projectile.aiStyle = -1;
             Projectile.friendly = true;
             Projectile.DamageType = DamageClass.Magic;
@@ -76,9 +90,27 @@ namespace InfernalEclipseWeaponsDLC.Content.Projectiles.MagicPro
 
         public override void AI()
         {
+            if (!storedVelocity)
+            {
+                originalSpeed = Projectile.velocity.Length();
+                storedVelocity = true;
+            }
             // Face the direction you're aiming (the velocity).
             if (Projectile.velocity.LengthSquared() > 0.0001f)
-                Projectile.rotation = Projectile.velocity.ToRotation() + RotationOffset;
+                Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+            // multiply speed but keep direction
+            if (speedTimer < 3)
+                speedTimer += 0.04f;
+            if (speedTimer == 3)
+                speedTimer = 3.1f;
+
+            float speedFactor = 0.1f * (float)(Math.Exp(speedTimer));
+            if (Projectile.velocity != Vector2.Zero)
+            {
+                Vector2 currentDir = Projectile.velocity.SafeNormalize(Vector2.UnitY);
+                Projectile.velocity = currentDir * originalSpeed * speedFactor;
+            }
 
             // Avoid horizontal mirroring when rotating.
             Projectile.spriteDirection = 1;
@@ -88,6 +120,24 @@ namespace InfernalEclipseWeaponsDLC.Content.Projectiles.MagicPro
             for (int i = 0; i < dusts.Length; i++)
                 Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, dusts[i],
                     Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 50, default, 1.35f).noGravity = true;
+
+            Vector3 blue = new Vector3(0.51f, 0.957f, 1f);
+            Vector3 red = new Vector3(1f, 0.255f, 0.392f);
+            Vector3 color = Vector3.Lerp(blue,red, Main.rand.NextFloat(0.3f, 0.7f));
+            Lighting.AddLight(Projectile.Center, color);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            for (int k = Projectile.oldPos.Length - 1; k > 0; k--)
+            {
+                Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+            }
+            return true;
         }
 
         public override bool OnTileCollide(Vector2 oldVelocity)
@@ -96,6 +146,104 @@ namespace InfernalEclipseWeaponsDLC.Content.Projectiles.MagicPro
             for (int index = 0; index < dusts.Length; ++index)
                 Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, dusts[index], oldVelocity.X * 0.2f, oldVelocity.Y * 0.2f, 100, Color.White, 1.25f);
             return true;
+        }
+    }
+    public class ArckaneScythe1Pro : ModProjectile
+    {
+        public ref float GemLocation => ref Projectile.ai[2];
+        private float oscillationTimer = 0f;
+        private float rotationTimer = 0f;
+        public override void SetDefaults()
+        {
+            Projectile.width = 20;
+            Projectile.height = 24;
+            Projectile.aiStyle = -1;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.penetrate = 4;
+            Projectile.timeLeft = 240;
+            Projectile.tileCollide = false;
+        }
+        public override void AI()
+        {
+            if (Projectile.velocity.LengthSquared() > 0.0001f)
+                Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+            oscillationTimer += (float)(Math.PI / 12);
+            if (oscillationTimer >= MathHelper.TwoPi)
+                oscillationTimer = 0;
+
+            rotationTimer += 0.5f;
+            Projectile.rotation = rotationTimer;
+            Projectile.velocity = 1.25f * Main.projectile[(int)GemLocation].velocity.RotatedBy(-Math.Sin(oscillationTimer));
+
+            if (!Main.projectile[(int)GemLocation].active)
+                Projectile.Kill();
+
+            Projectile.spriteDirection = 1;
+            Projectile.direction = 1;
+
+            Lighting.AddLight(Projectile.Center, 0.671f, 0.98f, 1f);
+
+            Dust splash = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<SulphuricSplash>(), Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 25, default, 1.35f);
+            splash.noGravity = true;
+            if (Main.rand.NextBool(3))
+            {
+                splash.scale *= 2f;
+                splash.velocity *= 2f;
+            }
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(ModContent.BuffType<CrushDepth>(), 300, false);
+        }
+
+    }
+    public class ArckaneScythe2Pro : ModProjectile
+    {
+        public ref float GemLocation => ref Projectile.ai[2];
+        private float oscillationTimer = 0f;
+        private float rotationTimer = 0f;
+        public override void SetDefaults()
+        {
+            Projectile.width = 20;
+            Projectile.height = 24;
+            Projectile.aiStyle = -1;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.penetrate = 4;
+            Projectile.timeLeft = 240;
+            Projectile.tileCollide = false;
+        }
+        public override void AI()
+        {
+            oscillationTimer += (float)(Math.PI / 12);
+            if (oscillationTimer >= MathHelper.TwoPi)
+                oscillationTimer = 0;
+
+            rotationTimer += 0.5f;
+            Projectile.rotation = rotationTimer;
+            Projectile.velocity = 1.25f * Main.projectile[(int)GemLocation].velocity.RotatedBy(Math.Sin(oscillationTimer));
+
+            if (!Main.projectile[(int)GemLocation].active)
+                Projectile.Kill();
+
+            Projectile.spriteDirection = 1;
+            Projectile.direction = 1;
+
+            Lighting.AddLight(Projectile.Center, 1f, 0.553f, 0.51f);
+
+            Dust fire = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<BrimstoneFlame>(),Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 50, default, 1.35f);
+            fire.noGravity = true;
+            if (Main.rand.NextBool(3))
+            {
+                fire.scale *= 2f;
+                fire.velocity *= 2f;
+            }
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(ModContent.BuffType<BrimstoneFlames>(), 300, false);
         }
     }
 }
